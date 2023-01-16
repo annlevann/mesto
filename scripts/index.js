03dc428785df0f328862183028344909eec43505
Anna Bekoeva <ann.bekoeva@yandex.ru>
2023-01-16 17:00:46 -0500

    fix

--- scripts/index.js
+++ scripts/index.js
@@ -5,14 +5,14 @@ const popupSaveButtonElement = popupElement.querySelector(".popup__save");
 
 let userInput = document.querySelector('.popup__input_text_user');
 let jobInput = document.querySelector('.popup__input_text_job');
-let userInputTitle = document.querySelector(".profile__title");
-let jobInputSubtitle = document.querySelector(".profile__subtitle");
+let userInputTitle = document.querySelector('.profile__title');
+let jobInputSubtitle = document.querySelector('.profile__subtitle');
 
 const openPopup = function() {
   popupElement.classList.add('popup_opened');
 
   userInput.value = userInputTitle.textContent;
-  jobInput.value = jobInput.textContent;
+  jobInput.value = jobInputSubtitle.textContent;
 }
 
 const closePopup = function() {
@@ -21,11 +21,11 @@ const closePopup = function() {
 
 const SaveButton = function(evt) {
   evt.preventDefault();
-  
+
   userInputTitle.textContent = userInput.value;
   jobInputSubtitle.textContent = jobInput.value;
 }
 
 popupOpenButtonElement.addEventListener('click', openPopup);
 popupCloseButtonElement.addEventListener('click', closePopup);
-popupSaveButtonElement.addEventListener('submit', handleFormSubmit);
+popupSaveButtonElement.addEventListener('submit', SaveButton);
