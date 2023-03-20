500656fa8be91051ea217affa16b55fc6947210a
Anna Bekoeva <ann.bekoeva@yandex.ru>
2023-03-20 14:13:44 -0400

    fix

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
-import Card from './Card.js';
-import FormValidator from "./FormValidate.js";
+import Card from '../components/Card.js';
+import FormValidator from "../components/FormValidate.js";
 
-import Section from "./Section.js";
-import Popup from "./Popup.js";
-import PopupWithForm from "./PopupWithForm.js";
-import PopupWithImage from "./PopupWithImage.js";
-import UserInfo from "./UserInfo.js";
+import Section from "../components/Section.js";
+import Popup from "../components/Popup.js";
+import PopupWithForm from "../components/PopupWithForm.js";
+import PopupWithImage from "../components/PopupWithImage.js";
+import UserInfo from "../components/UserInfo.js";
 
 const elementList = document.querySelector('.element__list');
 
